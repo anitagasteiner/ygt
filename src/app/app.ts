3b824bd4b332cafa4bd55a48e3aedda2ad11d87f
anitagasteiner <anita.gasteiner@posteo.de>
2026-01-19 17:03:22 +0100

    navbar component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Board } from './components/board/board';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Board
+    Board,
+    Navbar
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
