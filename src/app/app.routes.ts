cd96414b1b10acae202b55f5c04f765e82769f3b
anitagasteiner <anita.gasteiner@posteo.de>
2026-01-20 09:29:18 +0100

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Landingpage } from './components/landingpage/landingpage';
+import { Board } from './components/board/board';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Landingpage },
+    { path: 'board', component: Board }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Board } from './components/board/board';
 import { Navbar } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Board,
     Navbar
   ],
   templateUrl: './app.html',
