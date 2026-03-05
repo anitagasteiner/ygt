67e2b088d22433d950765da991d21390cd9ecfca
anitagasteiner <anita.gasteiner@posteo.de>
2026-03-05 13:40:02 +0100

    prepared showing invoices using toSignal

--- src/app/components/invoices/invoices.component.ts
+++ src/app/components/invoices/invoices.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { InvoiceComponent } from './invoice/invoice.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataBaseService } from '../../services/data-base.service';
+import { Invoice } from '../../models/invoice.model';
+import { InvoiceComponent } from './invoice/invoice.component';
+
 
 @Component({
   selector: 'app-invoices',
@@ -10,13 +15,52 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './invoices.component.html',
   styleUrl: './invoices.component.scss',
 })
-export class InvoicesComponent {
+export class InvoicesComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {}
+  /**
+   * Instance of DataBaseService used to access the Firebase Database.
+   * @type {DataBaseService}
+   */
+  dataBaseService: DataBaseService = inject(DataBaseService);
 
+
+  /**
+   * Observalbe stream of all invoices.
+   * @type {Observable<Invoice[]>}
+   */
+  invoices$: Observable<Invoice[]>;
+
+
+  invoicesTest = toSignal(this.dataBaseService.getData<Invoice>('invoices'), { initialValue: [] });
+
+
+  /**
+   * Creates an instance of the invoices component.
+   * @param route Provides access to information about the current route, including query parameters used to determine which invoices to display.
+   */
+  constructor(private route: ActivatedRoute) {
+    this.invoices$ = this.dataBaseService.getData<Invoice>('invoices');
+  }
+
+
+  /**
+   * Angular lifecycle hook that is called after the component has been initialized.
+   * Subscribes to the query parameters of the current route and XXXXXXXXXXXXXXXXX
+   */
   ngOnInit() {
-    const status = this.route.snapshot.queryParamMap.get('status');
+    this.route.queryParams.subscribe(params => {
+      this.showInvoices(params['status']);
+      // console.log(params['status']);
+    });
+  }
+
+  /**
+   * TODO
+   * @param status 
+   */
+  showInvoices(status: string) {
     console.log(status);
   }
+
 
 }
